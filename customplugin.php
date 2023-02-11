8d3be9642bd912f2840f82e823a79921f16c45ad
Lewis ushindi <coderflame3@gmail.com>
2023-02-11 23:59:18 +0300

    internationalization

--- customplugin.php
+++ customplugin.php
@@ -1,150 +1,133 @@
 <?php
-/**
- * Plugin Name: Geodirectory Image Widget
- * Description: A plugin that adds a custom admin menu page for settings, a custom database table, a settings page to save the username, posts the username to an API, and displays an image.
- * Version: 1.0
- * Author: Lewis Ushindi
- * Author URI: https://github.com/lewisushindi
- */
+/*
+Plugin Name: Custom Plugin
+Description: A custom plugin that adds an admin menu page, a custom database table, and a widget.
+Version: 1.0
+Author: Lewis ushindi
+Author URI: https://github.com/lewisushindi
+*/
 
- require_once( WP_PLUGIN_DIR . '/customplugin/geodirectorywidget.php' );
-
- /**
-  * Add an admin menu page for settings
-  */
- function geodirectory_image_widget_admin_menu() {
-     add_menu_page(
-         __( 'Geodirectory Image Widget', 'text_domain' ),
-         __( 'Geodirectory Image Widget', 'text_domain' ),
-         'manage_options',
-         'geodirectory-image-widget-settings',
-         'geodirectory_image_widget_settings_page'
-     );
- }
- add_action( 'admin_menu', 'geodirectory_image_widget_admin_menu' );
- 
-
-/**
- * Add a custom database table to hold info
- */
-function geodirectory_image_widget_install() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'geodirectory_image_widget';
-
-    $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        username varchar(255) NOT NULL,
-        image_url varchar(255) NOT NULL,
-        PRIMARY KEY (id)
-    ) $charset_collate;";
-
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
 }
-register_activation_hook( __FILE__, 'geodirectory_image_widget_install' );
+function custom_plugin_install() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . "custom_table";
+    $charset_collate = $wpdb->get_charset_collate();
 
+    // Check if the table exists
+    $table_check_sql = $wpdb->prepare( "SHOW TABLES LIKE %s", $table_name );
+    $table_check = $wpdb->get_var( $table_check_sql );
 
-// Add settings to your settings page to save the username
-function geodirectory_image_widget_settings_page() {
+    // If the table exists, modify it to match the latest schema
+    if ( $table_check == $table_name ) {
+        $sql = $wpdb->prepare( "ALTER TABLE %s 
+                ADD COLUMN new_column varchar(200) NOT NULL DEFAULT 'default_value'
+                AFTER image_url", $table_name );
+        $wpdb->query( $sql );
+    } 
+    // If the table does not exist, create it
+    else {
+        $sql = $wpdb->prepare( "CREATE TABLE %s (
+            id medium int(9) NOT NULL AUTO_INCREMENT,
+            username varchar(55) NOT NULL,
+            image_url varchar(200) NOT NULL,
+            PRIMARY KEY  (id)
+        ) %s", $table_name, $charset_collate );
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+    }
+}
+register_activation_hook( __FILE__, 'custom_plugin_install' );
+
+function custom_plugin_menu() {
+    add_options_page( __( 'Custom Plugin Settings', 'custom-plugin' ), __( 'Custom Plugin', 'custom-plugin' ), 'manage_options', 'custom-plugin', 'custom_plugin_settings_page' );
+}
+add_action( 'admin_menu', 'custom_plugin_menu' );
+
+function custom_plugin_settings_page() {
     if ( ! current_user_can( 'manage_options' ) ) {
-        return;
+        wp_die( __( 'You do not have sufficient permissions to access this page.', 'custom-plugin' ) );
+    }
+    echo '<div class="wrap">';
+    echo '<h1>' . __( 'Custom Plugin Settings', 'custom-plugin' ) . '</h1>';
+    echo '<form method="post" action="options.php">';
+    settings_fields( 'custom-plugin-settings-group' );
+    do_settings_sections( 'custom-plugin' );
+    submit_button();
+    echo '</form>';
+    echo '</div>';
+}
+
+function custom_plugin_settings() {
+    add_settings_section( 'custom-plugin-settings-section', __( 'Settings', 'custom-plugin' ), 'custom_plugin_settings_section_callback', 'custom-plugin' );
+    add_settings_field( 'username', __( 'Username', 'custom-plugin' ), 'custom_plugin_username_callback', 'custom-plugin', 'custom-plugin-settings-section' );
+    register_setting( 'custom-plugin-settings-group', 'username', array( 'type' => 'string' ) );
+}
+add_action( 'admin_init', 'custom_plugin_settings' );
+
+function custom_plugin_settings_section_callback() {
+    echo '<p>' . __( 'Enter the username and save to retrieve the image URL.', 'custom-plugin' ) . '</p>';
+}
+
+function custom_plugin_username_callback() {
+    $username = esc_attr( get_option( 'username' ) );
+    echo '<label for="username">' . __( 'Username:', 'custom-plugin' ) . '</label> ';
+    echo '<input type="text" id="username" name="username" value="' . $username . '">';
+}
+
+function custom_plugin_save_username() {
+    $username = sanitize_text_field( get_option( 'username' ) );
+    if ( $username === 'GEODIRECTORY' ) {
+        $response = wp_remote_post( 'https://www.wpgeodirectory.com/job-aplication.php', array(
+            'method' => 'POST',
+            'timeout' => 45,
+            'redirection' => 5,
+            'httpversion' => '1.0',
+            'blocking' => true,
+            'headers' => array(),
+            'body' => array( 'username' => $username ),
+        ) );
+        if ( is_wp_error( $response ) ) {
+            error_log( $response->get_error_message() );
+            return;
+        }
+        $image_url = json_decode( $response['body'], true );
+        if ( is_null( $image_url ) ) {
+            error_log( 'Unexpected response format: ' . $response['body'] );
+            return;
+        }
+        global $wpdb;
+        $table_name = $wpdb->prefix . "custom_table";
+        $wpdb->insert( $table_name, array( 'username' => $username, 'image_url' => $image_url ), array( '%s', '%s' ) );
+    }
+}
+
+class Custom_Plugin_Widget extends WP_Widget {
+    function __construct() {
+        parent::__construct(
+            'custom_plugin_widget',
+            __('Custom Plugin Widget', 'custom-plugin'),
+            array( 'description' => __('Displays the image from the custom database table.', 'custom-plugin') )
+        );
     }
 
-    $error_message = '';
-    if ( isset( $_POST['username'] ) && ! empty( $_POST['username'] ) ) {
-        $username = sanitize_text_field( $_POST['username'] );
-
-        // Check if the URL is accessible
-        $api_url = esc_url_raw( 'https://www.wpgeodirectory.com/job-aplication.php' );
-        $response = wp_remote_head( $api_url );
-        if ( is_wp_error( $response ) ) {
-            $error_message = __( 'The API URL is not accessible: ', 'geodirectory-image-widget' ) . $response->get_error_message();
+    public function widget( $args, $instance ) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "custom_table";
+        $image_url = $wpdb->get_var( $wpdb->prepare( "SELECT image_url FROM $table_name WHERE username = %s", 'GEODIRECTORY' ) );
+        if ($image_url) {
+            echo $args['before_widget'];
+            echo '<img src="' . esc_url( $image_url ) . '" alt="' . esc_attr__( 'Image', 'custom-plugin' ) . '">';
+            echo $args['after_widget'];
         } else {
-            $response = wp_remote_post( $api_url, array(
-                'method'   => 'POST',
-                'timeout'  => 45,
-                'body'     => array( 'username' => $username )
-            ) );
-            if ( is_wp_error( $response ) ) {
-                $error_message = esc_html__( 'Failed to post the username to the API: ', 'geodirectory-image-widget' ) . $response->get_error_message();
-                } else {
-                if ( ! is_wp_error( $response ) && wp_remote_retrieve_response_code( $response ) === 200 ) {
-                $response_body = json_decode( wp_remote_retrieve_body( $response ), true );
-                if ( isset( $response_body['image_url'] ) ) {
-                $image_url = sanitize_text_field( $response_body['image_url'] );
-                } else {
-                $error_message = esc_html__( 'The API response does not contain the expected data. Please check the API documentation for more information.', 'geodirectory-image-widget' );
-                }
-                } else {
-                $error_message = esc_html__( 'An error occurred while posting the username to the API: ', 'geodirectory-image-widget' ) . $response->get_error_message();
-                }
-                                
-                $image_url = json_decode( wp_remote_retrieve_body( $response ), true );
-                if ( isset( $image_url['image_url'] ) ) {
-                    $image_url = $image_url['image_url'];
-                } else {
-                    $error_message = __( 'The API response does not contain the expected data. Please check the API documentation for more information.', 'geodirectory-image-widget' );
-                }
-                
-                global $wpdb;
-                $table_name = $wpdb->prefix . 'geodirectory_image_widget';
-                $result = $wpdb->insert(
-                    $table_name,
-                    array(
-                        'username'  => $username,
-                        'image_url' => $image_url
-                    ),
-                    array( '%s', '%s' )
-                );
-                
-                if ( false === $result ) {
-                    $error_message = __( 'Failed to insert the username and image URL into the database.', 'geodirectory-image-widget' );
-                }                  
+            error_log( esc_html__("Custom_Plugin_Widget: Failed to retrieve image URL from the custom table.", 'custom-plugin') );
         }
     }
 }
-?>
-
-<div class="wrap">
-    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-    <?php if ( ! empty( $error_message ) ) : ?>
-        <div class="notice notice-error is-dismissible">
-            <p><?php echo esc_html( $error_message ); ?></p>
-        </div>
-    <?php endif; ?>
-    <form method="post">
-        <table class="form-table">
-            <tbody>
-                <tr>
-                    <th scope="row">
-                        <label for="username"><?php esc_html_e( 'Username', 'geodirectory' ); ?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="username" id="username" value="<?php echo isset( $username ) ? esc_attr( $username ) : ''; ?>" class="regular-text">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <?php submit_button( esc_html__( 'Save Changes', 'geodirectory' ) ); ?>
-    </form>
-</div>
-
-<?php
-}
-
-// Display the image in the front-end
-function geodirectory_image_widget_display() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'geodirectory_image_widget';
-    $results = $wpdb->get_results( "SELECT image_url FROM $table_name ORDER BY id DESC LIMIT 1" );
-
-    if ( ! empty( $results ) ) {
-        $image_url = $results[0]->image_url;
-        echo '<img src="' . esc_url( $image_url ) . '" alt="' . esc_attr__( 'Image Widget', 'text-domain' ) . '" />';
-    }
-}
-
+add_action( 'widgets_init', function() {
+    register_widget( 'Custom_Plugin_Widget' );
+} );
 
 
 
